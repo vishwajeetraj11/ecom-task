12dbc3f92fa61582d642856d7c859e69be049b9b
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 09:30:45 +0530

    added searchresults component

---
+++ src/components/searchResults/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import searchResultsStyles from "./searchResults.module.scss";
+
+type Props = {};
+
+const SearchResults = (props: Props) => {
+  return <div className={searchResultsStyles.searchResultsContainer}></div>;
+};
+
+export default SearchResults;

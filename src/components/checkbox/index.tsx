8955040abeadb5cb2fd84d842167192154dcf58a
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 22:06:54 +0530

    added support for onChange in checkbox

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -4,15 +4,17 @@ interface Props {
   name: string;
   label: React.ReactNode;
   id: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const CheckboxWithLabel = ({ name, label, id }: Props) => {
+export const CheckboxWithLabel = ({ name, label, id, onChange }: Props) => {
   return (
     <div className={checkboxStyles.checkboxContainer}>
       <input
         className={checkboxStyles.checkboxInput}
         type="checkbox"
         name={name}
+        onChange={onChange}
         id={id}
       />
       <label htmlFor={id} className={checkboxStyles.checkboxLabel}>

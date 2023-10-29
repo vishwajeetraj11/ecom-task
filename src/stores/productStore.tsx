b449c83824e7361960d9a3e64d76530036c2f478
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 17:59:37 +0530

    added wishlist store statea

--- src/stores/productStore.tsx
+++ src/stores/productStore.tsx
@@ -3,11 +3,15 @@ import { create } from "zustand";
 interface IProductStore {
   query: string;
   setQuery: (query: string) => void;
+  wishlist: string[];
+  setWishlist: (wishlist: string[]) => void;
 }
 
 const useProductStore = create<IProductStore>((set) => ({
   query: "",
   setQuery: (query: string) => set({ query }),
+  wishlist: [],
+  setWishlist: (wishlist: string[]) => set({ wishlist }),
 }));
 
 export default useProductStore;

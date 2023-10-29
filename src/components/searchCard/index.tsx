618af6312bd9b4bb253107c1c0d2f260b8446a17
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 10:42:22 +0530

    search results UI

---
+++ src/components/searchCard/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import searchCardStyles from "./searchCard.module.scss";
+
+type Props = {
+  title: string;
+  image: string;
+  discountedPrice: number;
+  price: number;
+  rating: number;
+  ratingsCount: number;
+  id: number;
+};
+
+const SearchCard = ({
+  discountedPrice,
+  image,
+  price,
+  rating,
+  ratingsCount,
+  title,
+}: Props) => {
+  return (
+    <div className={searchCardStyles.searchCardContainer}>
+      <div className={searchCardStyles.searchCardImageContainer}>
+        <img src={image} className={searchCardStyles.searchCardImage} />
+        <div className={searchCardStyles.searchCardViewProduct}>
+          <p>view product</p>
+        </div>
+      </div>
+      <p className={searchCardStyles.searchCardTitle}>{title}</p>
+      <div className={searchCardStyles.searchCardPriceContainer}>
+        <p className={searchCardStyles.searchCardDiscountedPrice}>
+          Rs {discountedPrice}
+        </p>
+        <p className={searchCardStyles.searchCardPrice}>Rs {price}</p>
+      </div>
+      <div className={searchCardStyles.searchCardRatingContainer}>
+        {/* <Rating /> */}
+        <p className={searchCardStyles.searchCardRatingsCount}>
+          {`(${ratingsCount})`}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SearchCard;
--- src/components/searchResults/index.tsx
+++ src/components/searchResults/index.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import searchResultsStyles from "./searchResults.module.scss";
+import SearchCard from "../searchCard";
+import { customFaker } from "../../shared/faker";
 
 type Props = {};
 
 const SearchResults = (props: Props) => {
-  return <div className={searchResultsStyles.searchResultsContainer}></div>;
+  const searchResults = [1, 2, 3, 4, 5, 6].map((result) => ({
+    title: customFaker.commerce.productName(),
+    image: customFaker.image.urlLoremFlickr({
+      category: "fashion",
+      height: 250,
+      width: 200,
+    }),
+    discountedPrice: customFaker.number.int({ min: 100, max: 800 }),
+    price: customFaker.number.int({ min: 200, max: 1000 }),
+    rating: customFaker.number.int({ min: 1, max: 5 }),
+    ratingsCount: customFaker.number.int({ min: 20, max: 200 }),
+    id: 1,
+  }));
+  return (
+    <div className={searchResultsStyles.searchResultsContainer}>
+      {searchResults.map((result) => (
+        <SearchCard key={result.id} {...result} />
+      ))}
+    </div>
+  );
 };
 
 export default SearchResults;

ff997628a9e335d31e1935897062eacb2486c397
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 18:00:27 +0530

    wishlist feat. UI + states wireup

--- src/components/searchCard/index.tsx
+++ src/components/searchCard/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import Rating from "../Rating";
 import searchCardStyles from "./searchCard.module.scss";
+import useProductStore from "../../stores/productStore";
 
 type Props = {
   title: string;
@@ -18,10 +20,39 @@ const SearchCard = ({
   rating,
   ratingsCount,
   title,
+  id,
 }: Props) => {
+  const { wishlist, setWishlist } = useProductStore();
+
+  const isWishlisted = wishlist.find((item) => item === id);
+
+  const onAddToWishlist = (id: string) => {
+    setWishlist([...wishlist, id]);
+  };
+
+  const onRemoveFromWishlist = (id: string) => {
+    const newWishlist = wishlist.filter((item) => item !== id);
+    setWishlist(newWishlist);
+  };
+
   return (
     <div className={searchCardStyles.searchCardContainer}>
       <div className={searchCardStyles.searchCardImageContainer}>
+        <div className={searchCardStyles.searchCardWishlist}>
+          {isWishlisted ? (
+            <AiFillHeart
+              onClick={() => onRemoveFromWishlist(id)}
+              size={20}
+              fill="#dc2626"
+            />
+          ) : (
+            <AiOutlineHeart
+              onClick={() => onAddToWishlist(id)}
+              size={20}
+              fill="#fff"
+            />
+          )}
+        </div>
         <img src={image} className={searchCardStyles.searchCardImage} />
         <div className={searchCardStyles.searchCardViewProduct}>
           <p>view product</p>
@@ -35,7 +66,7 @@ const SearchCard = ({
         <p className={searchCardStyles.searchCardPrice}>Rs {price}</p>
       </div>
       <div className={searchCardStyles.searchCardRatingContainer}>
-        {/* <Rating /> */}
+        <Rating rating={rating} />
         <p className={searchCardStyles.searchCardRatingsCount}>
           {`(${ratingsCount})`}
         </p>

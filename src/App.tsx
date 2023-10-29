56bc8aab56e03600fc619b8f1d85fee7c29274b9
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 22:06:04 +0530

    added brand logo

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import SearchResults from "./components/searchResults";
 function App() {
   return (
     <>
+      <img
+        width="92"
+        src="https://assets-global.website-files.com/622778f0460ef2a7b46117c1/632c38d0a82442dc8dfe0f53_zevi-logo-_2_.webp"
+        className="brandLogo"
+      />
       <SearchInput />
       <h3 className={"searchResultsLabel"}>Search Results</h3>
       <div className="searchFilterResultContainer">

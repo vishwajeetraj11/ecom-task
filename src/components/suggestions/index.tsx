f7b985304d0c2440080bdedfad00c3c010bc6532
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 18:00:00 +0530

    handle click outside for suggestions comtainer

--- src/components/suggestions/index.tsx
+++ src/components/suggestions/index.tsx
@@ -1,11 +1,32 @@
 import suggestionStyles from "./suggestions.module.scss";
 import { latestTrendsImages, popularSuggestions } from "../../shared/faker";
+import { useEffect } from "react";
 
 interface Props {}
 
 const Suggestions = (props: Props) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const suggestionsContainer = document.getElementById(
+        "searchSuggestionContainer"
+      );
+      if (
+        suggestionsContainer &&
+        !suggestionsContainer.contains(event.target as Node)
+      ) {
+        suggestionsContainer.style.display = "none";
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
-    <div className={suggestionStyles.suggestionsContainer}>
+    <div
+      id="searchSuggestionContainer"
+      className={suggestionStyles.suggestionsContainer}
+    >
       <p>Latest Trends</p>
       <div className={suggestionStyles.latestTrendsContainer}>
         {latestTrendsImages.map(({ image, title }) => {

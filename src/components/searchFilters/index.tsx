637ec3d9d6bfc22c36f1dceda70eba4c92904e04
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 09:30:34 +0530

    added search filters component

---
+++ src/components/searchFilters/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import searchFiltersStyles from "./searchFilters.module.scss";
+
+type Props = {};
+
+const SearchFilters = (props: Props) => {
+  return <div className={searchFiltersStyles.searchFiltersContainer}></div>;
+};
+
+export default SearchFilters;

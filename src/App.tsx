91e55e2e51e26eaf553f431f128ac9a291cbf318
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 09:30:50 +0530

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import "./App.css";
+import "./App.scss";
 import SearchInput from "./components/search";
+import SearchFilters from "./components/searchFilters";
+import SearchResults from "./components/searchResults";
 
 function App() {
   return (
     <>
       <SearchInput />
+      <h3 className={"searchResultsLabel"}>SearchResults</h3>
+      <div className="searchFilterResultContainer">
+        <SearchFilters />
+        <SearchResults />
+      </div>
     </>
   );
 }

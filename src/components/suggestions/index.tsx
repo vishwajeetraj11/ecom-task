d1896ba6a251b539cdac908b015044b84e503bcc
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 23:46:40 +0530

    popular suggestiosn clickable

--- src/components/suggestions/index.tsx
+++ src/components/suggestions/index.tsx
@@ -1,20 +1,23 @@
 import suggestionStyles from "./suggestions.module.scss";
-import { latestTrendsImages, popularSuggestions } from "../../shared/faker";
+import { latestTrendsImages, popularSuggestion } from "../../shared/faker";
 import { useEffect } from "react";
 import useProductStore from "../../stores/productStore";
 
 interface Props {}
-
 const Suggestions = (props: Props) => {
-  const { setSuggestionVisible } = useProductStore();
+  const { setSuggestionVisible, onSearch, setQuery } = useProductStore();
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       const suggestionsContainer = document.getElementById(
         "searchSuggestionContainer"
       );
-      if (suggestionsContainer) {
-        if (!suggestionsContainer.contains(event.target as Node)) {
+      const searchInput = document.getElementById("searchInput");
+      if (suggestionsContainer && searchInput) {
+        if (
+          !suggestionsContainer.contains(event.target as Node) &&
+          !searchInput.contains(event.target as Node)
+        ) {
           setSuggestionVisible(false);
         } else {
           setSuggestionVisible(true);
@@ -32,7 +35,7 @@ const Suggestions = (props: Props) => {
       id="searchSuggestionContainer"
       className={suggestionStyles.suggestionsContainer}
     >
-      <p>Latest Trends</p>
+      <p className={suggestionStyles.latestTrendsLabel}>Latest Trends</p>
       <div className={suggestionStyles.latestTrendsContainer}>
         {latestTrendsImages.map(({ image, title }) => {
           return (
@@ -46,8 +49,15 @@ const Suggestions = (props: Props) => {
       <p className={suggestionStyles.popularSuggestionsLabel}>
         Popular Suggestions
       </p>
-      {popularSuggestions.map((suggestion) => (
-        <p className={suggestionStyles.suggestionItem} key={suggestion}>
+      {popularSuggestion.map((suggestion) => (
+        <p
+          onClick={() => {
+            setQuery(suggestion);
+            onSearch();
+          }}
+          className={suggestionStyles.suggestionItem}
+          key={suggestion}
+        >
           {suggestion}
         </p>
       ))}

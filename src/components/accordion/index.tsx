c3710905d12e3264894fd64d954ee9644e200137
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-10-29 09:55:55 +0530

    added accordion

---
+++ src/components/accordion/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import accordionStyles from "./accordion.module.scss";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
+
+interface Props {
+  children: React.ReactNode;
+  title: string;
+  isOpen: boolean;
+  toggleAccordion: () => void;
+}
+
+const Accordion = ({ children, title, isOpen, toggleAccordion }: Props) => {
+  return (
+    <div className={accordionStyles.accordionContainer}>
+      <button
+        type="button"
+        onClick={toggleAccordion}
+        className={accordionStyles.accordionHeader}
+      >
+        <h3 className={accordionStyles.accordionTitle}>{title}</h3>
+        <div className={accordionStyles.accordionIcon}>
+          {isOpen ? <BiChevronUp /> : <BiChevronDown />}
+        </div>
+      </button>
+      <div className={accordionStyles.accordionContent}>{children}</div>
+    </div>
+  );
+};
+
+export default Accordion;
